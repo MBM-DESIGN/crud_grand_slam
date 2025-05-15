99226cb38056ee6bf5892190ccbdba222e640f6a
MBM-DESIGN <mariabelenmarchi@gmail.com>
2025-05-14 23:31:46 -0300

    creación del esquema y sus propiedades

--- src/index.ts
+++ src/index.ts
@@ -14,5 +14,26 @@ const connectMongoDb = async () => {
   }
 }
 
+interface IGrandSlamTournament {
+    name: 'Australian Open' | 'Roland-Garros' | 'Wimbledon' | 'US Open';
+    year: number;
+    surface: 'Hard' | 'Clay' | 'Grass';
+    location: 'Australia' | 'France' | 'England' | 'USA';
+    menSinglesChampion: string;
+    womenSinglesChampion: string;
+}
 
+const GrandSlamTournamentSchema = new Schema({
+  name: { type: String, required: true, unique: true, enum: ['Australian Open', 'Roland-Garros', 'Wimbledon', 'US Open'] },
+  year: { type: Number, required: true },
+  surface: { type: String, required: true, enum: ['Hard', 'Clay', 'Grass'] },
+  location: { type: String, required: true, enum: ['Australia', 'France', 'England', 'USA'] },
+  menSinglesChampion: { type: String; required: true },
+  womenSinglesChampion: { type: String; required: true }
+}, {
+  versionKey: false
+})
+
+
+//conexión a la BD
   connectMongoDb()
